9ad9868eac8efef1c07ded04b141559093ca62bb
Xue Yu <xueyublue@gmail.com>
2023-03-21 22:59:32 +0800

    Added cart items

--- shopping-cart/src/components/ShoppingCart.tsx
+++ shopping-cart/src/components/ShoppingCart.tsx
@@ -1,5 +1,26 @@
-import React from "react";
+import { Offcanvas, Stack } from "react-bootstrap";
+import { useShoppingCart } from "./../context/ShoppingCartContext";
+import { CartItem } from "./CartItem";
 
-export const ShoppingCart = () => {
-  return <div>ShoppingCart</div>;
+type ShoppingCartProps = {
+  isOpen: boolean;
+};
+
+export const ShoppingCart = ({ isOpen }: ShoppingCartProps) => {
+  const { closeCart, cartItems } = useShoppingCart();
+
+  return (
+    <Offcanvas show={isOpen} placement="end" onHide={closeCart}>
+      <Offcanvas.Header closeButton>
+        <Offcanvas.Title>Cart</Offcanvas.Title>
+      </Offcanvas.Header>
+      <Offcanvas.Body>
+        <Stack gap={3}>
+          {cartItems.map((item) => (
+            <CartItem key={item.id} {...item} />
+          ))}
+        </Stack>
+      </Offcanvas.Body>
+    </Offcanvas>
+  );
 };
--- shopping-cart/src/context/ShoppingCartContext.tsx
+++ shopping-cart/src/context/ShoppingCartContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, useContext, useState } from "react";
+import { ShoppingCart } from "../components/ShoppingCart";
 
 type ShoppingCartProviderProps = {
   children: React.ReactNode;
@@ -95,6 +96,7 @@ export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
       }}
     >
       {children}
+      <ShoppingCart isOpen={isOpen} />
     </ShoppingCartContext.Provider>
   );
 }
